692ba32ee45e5d13b1025bb9da59d8d442124d4c
Marvish Subhas Chandra <mschandr@uci.edu>
2022-10-25 10:08:19 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+// Marvish Chandra
+
+class bloodpressureWP{
+    function byProtocol(){
+        one = {AP: 94, OP: 90.5, D: 3.6};
+        five = {AP: 99.9, OP: 97.9, D: 2.0};
+        ten = {AP: 103.7, OP: 102.4, D: 1.3};
+        twentyfive = {AP: 110.6, OP: 109.4, D: 1.2};
+        fifty = {AP: 120.5, OP: 119.1, D: 1.3};
+        seventyfive = {AP: 131.9, OP: 130.9, D: 1.0};
+        ninety = {AP: 146.3, OP: 144.4, D: 1.9};
+        ninetyfive = {AP: 156.0, OP: 154.7, D: 1.3};
+        ninetynine = {AP: 176.5, OP: 176.2, D: 0.4};
+    }
+}
