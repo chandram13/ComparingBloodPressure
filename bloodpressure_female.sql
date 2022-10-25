4929cdf137470e813c3ed340aebeb30df691bdca
Marvish Subhas Chandra <mschandr@uci.edu>
2022-10-25 10:08:39 -0700

    Add files via upload

---
+++ bloodpressure_female.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE bloodpressurebyafem;
+
+CREATE TABLE comparingbloodpressure(
+    18-39years int,
+    40-59years int,
+    60years int
+);
+
+INSERT INTO comparingbloodpressure (18-39years, 40-59years, 60years)
+VALUES (110, 122, 139);
---
+++ bloodpressure_men.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE bloodpressurebymen;
+
+CREATE TABLE comparingbloodpressure(
+    18-39years int,
+    40-59years int,
+    60years int
+);
+
+INSERT INTO comparingbloodpressure (18-39years, 40-59years, 60years)
+VALUES (119, 124, 133);
